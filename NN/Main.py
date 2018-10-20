2a94b83f75408f44808decaa7e62c0abe6b4ee6f
huangyin0611 <1071324110@qq.com>
2018-10-20 20:27:57 +0800

    Initial commit

---
+++ NN/Main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lr_utils import load_dataset
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+# Example of a picture
+index = 25
+plt.imshow(train_set_x_orig[index])
+# plt.show()
+print("y = " + str(train_set_y[:, index]) + ", it is a " +
+      classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + " picture")
+print()
+
+### START CODE HERE ### (≈ 3 lines of code)
+m_train = train_set_y.shape[1]
+m_test = test_set_y.shape[1]
+num_px = train_set_x_orig.shape[1]
+### END CODE HERE ###
+print("Number of training examples: m_train = " + str(m_train))
+print("Number of testing examples: m_test = " + str(m_test))
+print("Height/Width of each image: num_px = " + str(num_px))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_set_x shape: " + str(train_set_x_orig.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x shape: " + str(test_set_x_orig.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+print()
+
+# Reshape the training and test examples
+### START CODE HERE ### (≈ 2 lines of code)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+### END CODE HERE ###
+print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
+
+# broadcast
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
+
+# sigma functions
+from Sigmoid import sigmoid
+
+print("sigmoid(0) = " + str(sigmoid(0)))
+print("sigmoid(9.2) = " + str(sigmoid(9.2)))
+
+# Initializing parameters
